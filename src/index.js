92588fcc9c5f8348f8b39a188a706592b37679e9
syzgod <viktor.berczeli@gmail.com>
2022-10-31 00:52:36 +0100

    UI changes

--- src/index.js
+++ src/index.js
@@ -32,10 +32,9 @@ function addBookToLibrary() {
 const addNewBook = document.querySelector(".add-new-book");
 addNewBook.addEventListener("click", () => {
   inputModal.style.display = "flex";
-  addNewBook.style.display = "none";
 });
 
-const addBookButton = document.querySelector(".add-book-btn");
+const addBookButton = document.querySelector(".add-book");
 addBookButton.addEventListener("submit", (e) => {
   e.preventDefault();
 });
@@ -44,39 +43,25 @@ const displayLibrary = () => {
   inputModal.style.display = "none";
   const libraryContainer = document.querySelector(".library-container");
   libraryContainer.innerHTML = "";
+  libraryContainer.style.display = "flex";
   myLibrary.forEach((book) => {
-    const bookContainer = document.createElement("div");
-    const bookTitle = document.createElement("h3");
-    const bookAuthor = document.createElement("p");
-    const bookDate = document.createElement("p");
-    const bookCategory = document.createElement("p");
-    const bookPages = document.createElement("p");
-    const bookRead = document.createElement("p");
-
-    bookContainer.classList.add("book-container");
-    bookTitle.classList.add("book-title");
-    bookAuthor.classList.add("book-author");
-    bookDate.classList.add("book-date");
-    bookCategory.classList.add("book-category");
-    bookRead.classList.add("book-read");
-    bookPages.classList.add("book-pages");
-
-    bookTitle.textContent = `Book title: ${book.title}`;
-    bookAuthor.textContent = `Book author: ${book.author}`;
-    bookDate.textContent = `Release date: ${book.date}`;
-    bookCategory.textContent = `In the category of ${book.category}`;
-    bookPages.textContent = `Number of pages: ${book.pages}`;
-    bookRead.textContent = `And the book is ${
-      book.read ? "already read" : "not read"
-    }`;
-
-    bookContainer.appendChild(bookTitle);
-    bookContainer.appendChild(bookAuthor);
-    bookContainer.appendChild(bookDate);
-    bookContainer.appendChild(bookCategory);
-    bookContainer.appendChild(bookPages);
-    bookContainer.appendChild(bookRead);
-    libraryContainer.appendChild(bookContainer);
+    const libraryContainer = document.querySelector(".library-container");
+    libraryContainer.innerHTML += `
+    <div class="book-container flex">
+      <h3 class="book-title">${book.title}</h3>
+      <div class="book-author">Book author: ${book.author}</div>
+      <div class="book-date">Release date: ${book.date}</div>
+      <div class="book-category">Category: ${book.category}</div>
+      <div class="book-pages">Number of pages: ${book.pages}</div>
+      <div class="book-read">The book is ${
+        book.read ? "already read" : "not read yet"
+      }</div>
+      <div class="card-buttons flex flex-row">
+        <button class="btn remove-book">Remove book</button>
+        <button class="btn change-read-status ml-3">Change read status</button>
+      </div>
+    </div>
+        `;
   });
 };
 
@@ -85,5 +70,4 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   addBookToLibrary();
   displayLibrary();
-  addNewBook.style.display = "block";
 });
